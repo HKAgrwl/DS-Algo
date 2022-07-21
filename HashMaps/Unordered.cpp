fb23bc947e6851d7b316a7295ed29a9a806c2aca
HKAgrwl <harshkumaragarwal2003@gmail.com>
2022-07-21 18:11:11 +0530

    21-July

---
+++ HashMaps/Unordered.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include <unordered_map>
+
+
+int main()
+{
+    unordered_map<string,int> ourMap;
+    // inserting
+    pair<string,int> p("abc",1);
+    ourMap.insert(p);
+    ourMap["def"] = 2;
+
+    // find or access
+    cout<<ourMap["abc"]<<endl;
+    cout<<ourMap.at("abc")<<endl;
+    // cout<<ourMap.at("ghi")<<endl;This gives an eror as "ghi" doea not map to anything.
+    cout<<ourMap["ghi"]<<endl;//This creates a new map with "ghi" as id and value as default value i.e 0
+
+    // Check Presence
+    if(ourMap.count("ghi")>0){
+        
+    }
+    return 0;
+}

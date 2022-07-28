94199bf6153456a36d77a8703c8b9c2055362ca8
HKAgrwl <harshkumaragarwal2003@gmail.com>
2022-07-28 23:00:46 +0530

    28-July-2

--- HashMaps/Implementation.cpp
+++ HashMaps/Implementation.cpp
@@ -70,7 +70,39 @@ class Map{
         }
         return hashcode% numBuckets;
     }
+
+    void rehash(){
+        MapNode<V>** temp = buckets;
+        buckets = new MapNode<V>*[2*numBuckets];
+        for(int i=0;i<2*numBuckets;i++){
+            buckets[i] = NULL;
+        }int oldBucketCount  = numBuckets;
+        numBuckets*=2;
+        count=0;
+
+        for(int i=0;i<oldBucketCount;i++)
+        {
+            MapNode<V>* head = temp[i];
+            while(head!=NULL){
+                string key = head->key;
+                V value = head->value;
+                insert(key,value);
+                head = head->next;
+            }
+        }
+        for(int i=0;i<oldBucketCount;i++){
+            MapNode<V>* head = temp[i];
+            delete(head);
+        }
+        delete[] temp;
+    }
+
     public:
+
+    double getLoadFactor(){
+        return (1.0*count/numBuckets);
+    }
+
     void insert(string key, V value){
         int bucketIndex = getBucketIndex(key);
         MapNode<V>* head = buckets[bucketIndex];
@@ -86,6 +118,10 @@ class Map{
         node->next = head;
         buckets[bucketIndex] = node;
         count++;
+        double loadFactor = (1.0* count/numBuckets);
+        if(loadFactor > 0.7){
+            rehash();
+        } 
     }
     V remove(string key){
         int bucketIndex = getBucketIndex(key);
@@ -114,6 +150,23 @@ class Map{
 
 int main()
 {
-    
+    Map<int> map;
+    for(int i=0;i<10;i++){
+        char c = '0' + i;
+        string key = "abc";
+        key = key+c;
+        int value = i+1;
+        map.insert(key,value);
+        cout<<map.getLoadFactor()<<endl;  
+    }
+    map.remove("abc2");
+    map.remove("abc7");
+    for(int i=0;i<10;i++){
+        char c = '0' + i;
+        string key = "abc";
+        key = key+c;
+        cout<<key<<" : "<<map.getValue(key)<<endl;
+    }
+    cout<<map.size()<<endl;
     return 0;
 }
--- HashMaps/MapUse.cpp
+++ HashMaps/MapUse.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<int> removeDuplicate(int* a,int size){
     vector<int> output;
-    unordered_map<int,bool> seen;
+    unordered_map<int,bool> seen;   
     for (int i = 0; i < size; i++)
     {
         if(seen.count(a[i])>0){
@@ -57,6 +57,70 @@ int getPairs(int* arr,int size,int sum){
     return twice_count/2;
 }
 
+int diffK(int* arr,int size,int k){
+    unordered_map<int,int> m;
+    for(int i=0;i<size;i++){
+        m[arr[i]]++;
+    }
+    int count=0;
+    for(int i=0;i<size;i++){
+        count+=m[arr[i]+k]*m[arr[i]];
+        count+=m[arr[i]-k]*m[arr[i]];
+        m[arr[i]]=0;
+    }
+    return count;
+}
+
+pair<int,int> Longestsubseq(int* arr,int size){
+    pair<int,int> p;
+    p.first = p.second = 0;
+    unordered_map<int,int> m;
+    for(int i=0;i<size;i++){
+        m[arr[i]]++;
+    }
+    int j,len,max,start,end;
+    len=max=1;
+    j=1;
+    for(int i=0;i<size;i++){
+        while(m.count(arr[i]+j)>0){
+            len++;
+            j++;
+        }
+        end = arr[i]+j-1;
+        j=1;
+        while(m.count(arr[i]-j)>0){
+            len++;
+            j++;
+        }
+        start = arr[i]-j+1;
+        if(len>max){
+            max=len;
+            p.first= start;
+            p.second = end;
+        }
+    }
+    return p;
+}
+
+int zeroSum(int* arr,int size){
+    unordered_map<int,int> hashmap;
+    int max,len,sum;
+    max=len=sum=0;
+    for(int i=0;i<size;i++){
+        sum+= arr[i];
+        if(hashmap[sum] > 0){
+            len = i - hashmap[sum] + 1;
+            if(len>max){
+                max=len;
+            }
+            continue;
+        }
+        hashmap[sum]=i+1;
+    }
+    return max;
+
+}
+
 int main()
 {
     // int a[] = {1,2,3,3,2,1,4,3,6,5,5};
@@ -64,12 +128,16 @@ int main()
     // for(int i=0;i<output.size();i++){
     //     cout<<output[i]<<endl;
     // }
-    int n;
+    int n,k;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<maxFreq(arr,n);
+    // cin>>k;
+    // cout<<diffK(arr,n,k);
+    // pair<int,int> p = Longestsubseq(arr,n);
+    // cout<<p.first<<" "<<p.second;
+    cout<<zeroSum(arr,n);
     return 0;
 }

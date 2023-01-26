55be65744d5b7802f3de906fabe65e648a0c2a43
HKAgrwl <harshkumaragarwal2003@gmail.com>
2023-01-26 17:57:46 +0530

    26Jan

--- Graphs/graph.cpp
+++ Graphs/graph.cpp
@@ -131,8 +131,8 @@ bool isCyclicDFS(int node, int parent,unordered_map<int,bool> &visited,unorderd_
         if(!visited[neighbour]){
             return isCyclicDFS(neighbour,node,visited,adj);
         }
-        else if(neoghbour!=parent){
-            return true;
+        else if(neighbour!=parent){
+            return true;    
         }
     }
 }
@@ -143,11 +143,184 @@ string cycleDetection(vector<vector<int>> edges,int n,int m){
     prepareAdjList(adjList,edges);
     for(int i=0;i<n;i++){
         if(!visited[i]){
-            bool ans = isCyclicBFS(i,visited,asjList);
+            bool ans = isCyclicDFS(i,-1,visited,adjList);
             if(ans==1) return "YES";
         }
     }
     return "NO";
+}
+
+void topoSort(int node, vector<bool> &visited,stack<int> &s,unordered_map<int,list<int>> &adj;){
+    visited[node] = 1;
+    for(auto neighbour:adj[node]){
+        if(!visited[neighbour])
+            topoSort(neighbour,visited,s,adj);
+    }
+    // imp
+    s.push(node);
+}
+
+vector<int> topologicalSort(vector<vector<int>> &edges, int v,int e){
+    // create adj list
+    unordered_map<int,list<int>> adj;
+    for(int i=0;i<e;i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    // call dfs topological sort util function for all components
+    vector<bool> visited;
+    stack<int> s;
+    for(int i =0;i<n;i++){
+        if(!visited[i]){
+            topoSort(i,visited,s,adj);
+        }   
+    }   
+
+    vector<int> ans;
+    while(!s.empty()){
+        ans.push_back(s.top());
+    }
+}
+
+// Kahn's algo for topologocal sort
+vector<int> topolopgicalSortKahn(vector<vector<int>> &edges,int v,int e){
+    // create adj list
+    unordered_map<int,list<int>> adj;
+    for(int i=0;i<e;i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    // find all indegees
+    vector<int> indegree(v);
+    for(auto i:adj){
+        for(auto j:i.second){
+            indegree[j]++; 
+        }
+    }
+    // push those with 0 indegree
+    queue<int> q;
+    for(int i=0;i<v;i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+    // do bfs
+    vector<int> ans;
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+
+        // ans store
+        ans.push_back(front);
+
+        // neighbour indregree update
+        for(auto neighbour:adj[front]){
+            indegree[neighbour]--;
+            if(indegree[neighbour]==0){
+                q.push(neighbour);
+            }
+        }
+    }
+    return ans;
+}
+
+vector<int> shortestPath(vector<pair<int,int>> edges,int n,int m,int s,int t){
+    // create adjList;
+    unordered_map<int,list<int>> adj;
+    for(int i=0;i<edges.size();i++){
+        int u = edges[i].first;
+        int v = edges[i].second;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    // do bfs
+    unordered_map<int,bool> visited;
+    unordered_map<int,int> parent;
+    parent[s]=-1;
+    q.push(s);
+    visited[s]=true;
+
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+
+        for(auto i : adj[front]){
+            if(!visited[i]){
+                visited[i]=true;
+                parent[i]=front;
+                q.push(i);
+            }
+        }
+    }
+
+    // prepare shortest path
+    vector<int> ans;
+    int currentNode = t;
+    while(currentNode!=s){
+        currentNode = parent[currentNode];
+        ans.push_back(currentNode);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+
+
+vector<int> dijkstra(vector<vector<int>> &vec,int vertices,int edges,int sources){
+    // create adjascency list
+    unordered_map<int,list<pair<int,int>>> adj;
+    for(int i=0;i<edges;i++){
+        int u = vec[i][0];
+        int v = vec[i][1];
+        int w = vec[i][2];
+
+        adj[u].push_back(make_pair(v,w));
+        adj[v].push_back(make_pair(u,w));
+    }
+
+    vector<int> dist(vertices);
+    for(int i=0;i<vertices;i++){
+        dist[i] = INT_MAX;
+    }
+    set<pair<int,int>> st;
+
+    dist[source] = 0;
+    st.insert(make_pair(0,source));
+
+    while(!st.empty()){
+        // fetch top record
+        auto top = *(st.begin());
+        int nodeDistance = top.first;
+        int topNode = top.second();
+
+        // remove top new record
+        st.erase(st.begin());
+
+        // traverse on neighbours
+        for(auto neighbour : adj[topNode]){
+            if(nodeDistance +neighbour.second < dist[neighbour.first]){
+                auto record = st.find(make_pair(dist[neighbour.first],neighbour.first));
+                // if record found then erase it
+                if(record != st.end()){
+                    st.erase(record);
+                }
+
+                // distance update
+                dist[neighbour.first] = nodeDistance + neighbour.second;
+                // record push in set
+                st.insert(make_pair(dist[neighbour.first],neighbour.first));
+            }
+        }
+    }
+    return dist;
 }
 
 int main()

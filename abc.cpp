c6b22b97c35187a96032f9fe65adee27d11ca450
HKAgrwl <harshkumaragarwal2003@gmail.com>
2023-04-28 12:58:41 +0530

    28April

---
+++ abc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string rawInput;
+    // vector<string> numbers;
+    // while (getline(cin, rawInput, ' '))
+    // {
+    //     numbers.push_back(rawInput);
+    // }
+    // for(int i=0;i<numbers.size();i++){
+    //     cout<<numbers[i]<<endl;
+    // }
+    
+    return 0;
+}
--- xyz.cpp
+++ xyz.cpp
@@ -1,38 +1,37 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
-
-string helperFunc(string x, int n, int k);
 
 int main()
 {
-	int t;
-	cin >> t;
-	cout << helperFunc("1", 1, t);
+	while(cin){
+		vector<int> arr;
+		while(cin.peek()!='\n'){
+		int x;
+		cin>>x;
+		arr.push_back(x);
+		}
+	}
+	
+
+	int  l = arr[arr.size()-1];
+	arr.pop_back();
+	sort(arr.begin(),arr.end());
+	int n=0;    
+	int left = 0;
+	int right = arr.size()-1;
+	while(left<right){
+		int residue = l - arr[right];
+		while(residue >= arr[left]){
+			left++;
+			residue-= arr[left];
+		}
+		n++;
+		right--;
+	}
+	if(right==left)cout<<n+1;
+	else cout<<n;
 	return 0;
 }
 
-string helperFunc(string x, int n, int k)
-{
-	if (n == k)
-	{
-		return x;
-	}
-	string str = "";
-	int count = 0;
-	int left = 0;
-	int right = 0;
-	while (right < x.size())
-	{
-		while (right < x.size() && (x[left] == x[right]))
-		{
-			right++;
-			count++;
-		}
-		left=right;
-		string cnt = to_string(count);
-		str += cnt;
-		str += x[left-1];
-		count=0;
-	}
-	return helperFunc(str, n+=1, k);
-}

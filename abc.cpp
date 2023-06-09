2c98f34e07b50fae3ab48b275e5caaa32976ba11
Harsh <harshkumaragarwal2003@gmail.com>
2023-06-09 17:21:23 +0530

    6June

--- abc.cpp
+++ abc.cpp
@@ -1,18 +1,9 @@
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    // string rawInput;
-    // vector<string> numbers;
-    // while (getline(cin, rawInput, ' '))
-    // {
-    //     numbers.push_back(rawInput);
-    // }
-    // for(int i=0;i<numbers.size();i++){
-    //     cout<<numbers[i]<<endl;
-    // }
     
+    cout<<"hello"<<endl;
     return 0;
 }
